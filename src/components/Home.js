83c9485bf76ffffcd7a015ff742b92d3cc086c6b
Parimal Ingle <parimal.ingle18@gmail.com>
2023-05-06 14:41:07 +0530

    fixing image issue try - 4

--- src/components/Home.js
+++ src/components/Home.js
@@ -200,7 +200,7 @@ const GridItem3 = styled(GridItem)`
 `;
 
 const ProjectIcon = styled.img`
-  content: url('personal-portfolio-react/home_profile.jpeg');
+  content: url('/personal-portfolio-react/projectIcon.png');
   height: 10.7em;
   border-radius: 30px;
   margin: 0em 2em 1em 2em;
@@ -214,7 +214,7 @@ const GridItem4 = styled(GridItem)`
 `;
 
 const CertificationIcon = styled.img`
-  content: url('personal-portfolio-react/certificationIcon.jpeg');
+  content: url('/personal-portfolio-react/certificationIcon.png');
   height: 10em;
   width: 10em;
   border-radius: 30px;
@@ -230,7 +230,7 @@ const GridItem5 = styled(GridItem)`
 `;
 
 const WorkExpIcon = styled.img`
-  content: url('personal-portfolio-react/workExpIcon.jpeg');
+  content: url('/personal-portfolio-react/workExpIcon.png');
   height: 10em;
   width: 10em;
   border-radius: 30px;
